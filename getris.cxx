01ee7808312e91b28c8d83ee864688ff404ebe00
iloveooz <iloveooz@yandex.ru>
2018-12-11 13:56:09 +0700

    Added images

--- getris.cxx
+++ getris.cxx
@@ -1,2 +1,24 @@
 
 	// clone of tetris
+
+	#include <SFML/Graphics.hpp>
+	#include <time.h>
+	
+	int main() {
+		sf::RenderWindow window(sf::VideoMode(320, 480), "Getris!");
+		
+		while (window.isOpen()) {
+			sf::Event event;
+			
+			while (window.pollEvent(event)) {
+				if (event.type == sf::Event::Closed)
+				window.close();
+				
+			}
+			window.clear(sf::Color::White);
+			window.display();
+		
+		}
+		
+		return 0;
+	}
